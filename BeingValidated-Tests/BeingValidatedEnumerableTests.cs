3b22ad68568b76c539673f59429e2bd3e1c0207e
KevinHuang <xiangwei.326@gmail.com>
2023-06-27 09:11:06 +0800

    Add: Unit test for async method

--- BeingValidated-Tests/BeingValidatedEnumerableTests.cs
+++ BeingValidated-Tests/BeingValidatedEnumerableTests.cs
@@ -8,7 +8,7 @@ namespace BeingValidated_Tests
     public class BeingValidatedEnumerableTests
     {
         [Test]
-        public async Task Validate_WillCheckOnExceptionIsNull_AndSetToDefaultBehaviour()
+        public void Validate_WillCheckOnExceptionIsNull_AndSetToDefaultBehaviour()
         {
             // Make sure that when OnException is passed as null,
             // exceptions are thrown instead of silently ignored.
@@ -19,7 +19,27 @@ namespace BeingValidated_Tests
 
             // Act
             Exception actual = Assert.Catch<Exception>(() => testString.StartValidateElements()
-                .Validate(s => throw new Exception(exceptionContent.ToString())));
+                .Validate(_ => throw new Exception(exceptionContent.ToString())));
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOf<Exception>(actual);
+            Assert.AreEqual(exceptionContent.ToString(), actual.Message);
+        }
+
+        [Test]
+        public async Task ValidateAsync_WillCheckOnExceptionIsNull_AndSetToDefaultBehaviour()
+        {
+            // Make sure that when OnException is passed as null,
+            // exceptions are thrown instead of silently ignored.
+
+            // Arrange
+            string testString = "I am funny!";
+            int exceptionContent = new Random().Next();
+
+            // Act
+            Exception actual = Assert.Catch<Exception>(() => testString.StartValidateElements()
+                .ValidateAsync(async _ => await Task.FromException(new Exception(exceptionContent.ToString()))));
 
             // Assert
             Assert.IsNotNull(actual);
