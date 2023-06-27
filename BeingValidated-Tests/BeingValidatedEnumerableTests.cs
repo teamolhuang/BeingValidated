1a2164f7d4e1923058a77a9084c3be0a88ab2a72
KevinHuang <xiangwei.326@gmail.com>
2023-06-27 09:04:49 +0800

    Add: Unit test project

---
+++ BeingValidated-Tests/BeingValidatedEnumerableTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using BeingValidated;
+using NUnit.Framework;
+
+namespace BeingValidated_Tests
+{
+    public class BeingValidatedEnumerableTests
+    {
+        [Test]
+        public async Task Validate_WillCheckOnExceptionIsNull_AndSetToDefaultBehaviour()
+        {
+            // Make sure that when OnException is passed as null,
+            // exceptions are thrown instead of silently ignored.
+
+            // Arrange
+            string testString = "I am funny!";
+            int exceptionContent = new Random().Next();
+
+            // Act
+            Exception actual = Assert.Catch<Exception>(() => testString.StartValidateElements()
+                .Validate(s => throw new Exception(exceptionContent.ToString())));
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOf<Exception>(actual);
+            Assert.AreEqual(exceptionContent.ToString(), actual.Message);
+        }
+    }
+}
